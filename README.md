9806bd97180de3248055d4cdf127136a71c69a44
Wilson Liao <droyuki@gmail.com>
2021-06-16 11:22:09 +0800

    feat: README.md

---
+++ README.md
@@ -0,0 +1,86 @@
+# gofer
+> A gofer for devops developer
+
+
+![](header.png)
+
+## Installation
+Donwload latest [Release](https://github.com/droyuki/gofer/releases)
+
+### MacOS
+```
+mv gofer-macos /usr/local/bin/gofer
+```
+### Linux
+```
+mv gofer-linux /usr/local/bin/gofer
+```
+### Wondows
+Run gofer-windows.exe in CMD
+
+## Usage example
+
+```
+# Display help message
+gofer -h
+
+Usage: gofer [options] [command]
+
+Options:
+  --init [path]         create config.json to <path>
+  -c, --config <path>  config file
+  -v, --version        output the version number
+  -h, --help           display help for command
+
+Commands:
+  gitlab               execcute gitlab tool
+  docker             execcute docker tool
+  k8s                  execcute k8s tool
+  help [command]       display help for command
+
+```
+
+## Config.json
+
+Create config.json at the first time
+```
+gofer init [path]
+
+```
+
+config.json
+```json
+{
+    "gitlab-api": "https://gitlab.com/api/v4",
+    "gitlab-token": "YOUR_GITLAB_TOKEN",
+    "projects": []
+}
+```
+
+
+
+
+## Development setup
+
+All you need is nodejs, then run npm install
+
+```sh
+npm install
+```
+
+## Release History
+
+* 1.0.0
+    * Support gitlab
+
+## Meta
+
+Distributed under the MIT license.
+
+## Contributing
+
+1. Fork it (<https://github.com/droyuki/gofer/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
--- gofer.js
+++ gofer.js
@@ -3,60 +3,69 @@
 const clear = require('clear');
 const fs = require('fs');
 const { Command } = require('commander');
-
 const logger = require('./lib/logger');
 const { getAction, selectProject } = require('./lib/question');
-
 const handler = require('./lib/handler');
 
 function parseArgs() {
   const program = new Command();
 
   program
-    .option('--init [path]', ' create config.json to <path>')
     .option('-c, --config <path>', 'config file')
     .version('1.0.0', '-v, --version');
 
-  program.command('gitlab').description('execcute gitlab tool');
-  program.command('docker').description('execcute docker tool');
-  program.command('k8s').description('execcute k8s tool');
+  program
+    .command('init [path]')
+    .description('create config.json to [path]');
+
+  program
+    .command('gitlab')
+    .description('execcute gitlab tool');
+
+  program
+    .command('docker')
+    .description('execcute docker tool');
+
+  program
+    .command('k8s')
+    .description('execcute k8s tool');
+
   program.parse();
 
-  const [action] = program.args;
+  const [action, initConfigPath] = program.args;
   const opts = program.opts();
 
-  return { action, ...opts };
+  return { action, initConfigPath, ...opts };
+}
+
+function initConfig(initConfigPath) {
+  const configFile = initConfigPath || `${process.cwd()}/config.json`;
+
+  logger.log(`Creating ${configFile}`);
+
+  const content = {
+    'gitlab-api': 'https://gitlab.com/api/v4',
+    'gitlab-token': 'YOUR_GITLAB_TOKEN',
+    workspace: '',
+    projects: [],
+  };
+
+  fs.writeFileSync(
+    configFile,
+    JSON.stringify(content, null, 4),
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  process.exit();
 }
 
 function getConfig(opts) {
-  const { init, config } = opts;
-
-  if (init) {
-    const configFile =
-      init === true ? `${process.cwd()}/config.json` : init;
-
-    logger.log(`Creating ${configFile}`);
-
-    const content = {
-      'gitlab-api': 'https://gitlab.com/api/v4',
-      'gitlab-token': 'YOUR_GITLAB_TOKEN',
-      workspace: '',
-      projects: [],
-    };
-
-    fs.writeFileSync(
-      configFile,
-      JSON.stringify(content, null, 4),
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      }
-    );
-
-    process.exit();
-  }
+  const { config } = opts;
 
   if (!config) {
     logger.error('Config not found!');
@@ -69,7 +78,12 @@ function getConfig(opts) {
 
 async function main() {
   logger.logo();
-  const { action, ...opts } = parseArgs();
+  const { action, initConfigPath, ...opts } = parseArgs();
+
+  if (action === 'init') {
+    initConfig(initConfigPath);
+  }
+
   const config = getConfig(opts);
   const { projects } = await selectProject(config.projects);
 
